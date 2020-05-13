3614aa26c1967a57ac6cf813270172142eb23795
Christopher Violante <christopherviolante@Christophers-MacBook-Pro.local>
2020-05-13 11:41:47 -0600

    game logic done, css finishing but not done yet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // /*----- constants -----*/
-// const deck in other JS is entire deck of cards
 const score = {
     playerScore: 0,
     opponentScore: 0
@@ -7,21 +6,19 @@ const score = {
 
 
 // /*----- app's state (variables) -----*/
-// use a board array to represent the cards in play
 let round;
 let playerCards = [];
 let opponentCards = [];
 let shuffledDeck = [];
 let playerCard;
 let opponentCard;
+let tieCards = [];
 
 // /*----- cached element references -----*/
 warBtn = document.getElementById('war');
 newGameBtn = document.getElementById('newGame');
 statusBar = document.querySelector('#status');
-// cache the win round counter
-// cache each theme option
-
+document.querySelector('#infoBar').innerHTML = `Player Cards: ${playerCards.length}, Opponent Cards: ${opponentCards.length}`
 
 
 // /*----- event listeners -----*/
@@ -49,40 +46,59 @@ function render() {
     }
        playCards();
    if(playerCard.value > opponentCard.value) {
-        statusBar.innerHTML = "Player wins this round!"
+        statusBar.innerHTML = `Player wins this round!`
+        document.querySelector('#infoBar').innerHTML = `Player Cards: ${playerCards.length}, Opponent Cards: ${opponentCards.length}`
    } else if(opponentCard.value > playerCard.value) {
-    statusBar.innerHTML = "Opponent wins this round!"
+    statusBar.innerHTML = `Opponent wins this round!`
+    document.querySelector('#infoBar').innerHTML = `Player Cards: ${playerCards.length}, Opponent Cards: ${opponentCards.length}`
    } else if (opponentCard.value = playerCard.value) {
-       statusBar.innerHTML = "Its a tie! WAR!!!"
+       statusBar.innerHTML = `Its a tie! WAR!!!`
+       document.querySelector('#infoBar').innerHTML = `Player Cards: ${playerCards.length}, Opponent Cards: ${opponentCards.length}`
    }
    checkForWinner();
 };
 
 function checkForWinner() {
-       if(playerCard.value > opponentCard.value) {
-        score.playerScore += 1
+    if(playerCard.value > opponentCard.value) {
+        score.playerScore += 1;  
+        if(tieCards.length > 0) {
+            playerCards.push(...tieCards);
+            tieCards = [];
+        } else{
+            playerCards.push(playerCard);
+            playerCards.push(opponentCard);
+        }
     }else if (playerCard.value < opponentCard.value) {
-        score.opponentScore += 1
+        score.opponentScore += 1;
+        if(tieCards.length > 0) {
+            opponentCards.push(...tieCards);
+            tieCards = [];
+        } else{
+            opponentCards.push(playerCard);
+            opponentCards.push(opponentCard);
+        }
     }else if (playerCard.value === opponentCard.value) {
+        tieCards.push(playerCard, opponentCard);
         for(i=1; i <= 4; i++) {
-            playCards();
+            playCards(true);
         } 
         checkForWinner();
     }
 }
 
-function playCards() {
+function playCards(isTie) {
     playerCard = playerCards.shift();
     opponentCard = opponentCards.shift();
     document.querySelector('#player').setAttribute('class', playerCard.name);
     document.querySelector('#opponent').setAttribute('class', opponentCard.name);
+    if (isTie) {
+        tieCards.push(playerCard, opponentCard);
+    }
 };
 
 
 
-// check for winner function that determines which card is a higher value and declares that player who has that card the winner of the round
-// move cards of hand played to the end of the winners cards
-// add 1 to the won rounds counter of the player that won the hand
+
 // play celebratory or dissapointing sound or tie sound
-// return back to render function for next hand
+
 
